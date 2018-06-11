939ed755fd60a4f2aeb8cdcf5cde58c6d8f845c0
ravindu14 <ravindulandekumbura14@gmail.com>
2018-06-12 00:47:27 +0530

    ReactFrontend is commited
    make different dirrectories and work on your own dirrectory

---
+++ AfReactFrontend/AppContainer.jsx
@@ -0,0 +1,11 @@
+'use strict';
+import React, {Component}   from 'react';
+
+export default class AppContainer extends Component {
+
+    render(){
+        return <div>
+            <h2>code here</h2>
+        </div>
+    }
+}
