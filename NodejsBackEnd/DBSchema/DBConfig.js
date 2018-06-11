ca7a8052facce65b6bc9d48161e47287fe45d49b
Dharshika <dharshikasingarathnam@gmail.com>
2018-06-11 16:51:32 +0530

    wardSchema.js added

--- NodejsBackEnd/DBSchema/DBConfig.js
+++ NodejsBackEnd/DBSchema/DBConfig.js
@@ -1,9 +1,16 @@
-'use strict'
-
 const mongoose = require('mongoose');
+const wardSchema = require('./wardSchema');
+;
+
+mongoose.connect('mongodb://127.0.0.1:27017/Inward',err =>{
+    if(err) {
+        console.error(err);
+        process.exit(-2);
+    }
+    console.log('Connected to mongoDB on PORT 27017');
+});
+
+mongoose.model('wardDetails',wardSchema);
 
 
-
-
-
-mongoose.connect();
+module.exports = mongoose;
