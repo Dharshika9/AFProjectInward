b3a99297dc30faa087c465c250856125a67f9623
ravindu14 <ravindulandekumbura14@gmail.com>
2018-06-14 02:06:34 +0530

    Login backend

--- NodejsBackEnd/routes.js
+++ NodejsBackEnd/routes.js
@@ -5,9 +5,11 @@ const router = express.Router();
 
 const wardRoute = require('./ApplicationRoutes/ward.routes');
 const userRoute = require('./ApplicationRoutes/user.routes');
+const loginRoute = require('/ApplicationRoutes/login.routes');
 
 
 router.use('/wards/' , wardRoute);
 router.use('/user/' , userRoute);
+router.use('/login/' , loginRoute);
 
 module.exports = router;
