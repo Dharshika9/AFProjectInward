ecdf976e1fbb4dea35963a95efb6b7a5ce081e9f
Dharshika <dharshikasingarathnam@gmail.com>
2018-06-15 00:34:53 +0530

    Ward Components changed

--- NodejsBackEnd/DBSchema/DBConfig.js
+++ NodejsBackEnd/DBSchema/DBConfig.js
@@ -5,10 +5,6 @@ const patientAdmissionSchema = require('./patientWardAdmissionSchema');
 const patientRegSchema = require('./patientRegistrationSchema');
 
 const userSchema = require('./userSchema');
-<<<<<<< HEAD
-=======
-
->>>>>>> 6c7d1267fd2d085a91077ed160db4025cd483637
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/Inward', err =>{
@@ -24,13 +20,8 @@ mongoose.model('wardDetails',wardSchema);
 mongoose.model('patientWardAdmission',patientAdmissionSchema);
 mongoose.model('patientRegistration',patientRegSchema);
 
-
-<<<<<<< HEAD
-mongoose.model('User' , userSchema);
-=======
-
 mongoose.model('User' , userSchema);
 
->>>>>>> 6c7d1267fd2d085a91077ed160db4025cd483637
+
 
 module.exports = mongoose;
